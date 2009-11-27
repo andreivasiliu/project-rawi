54b93e03c85490bc99f9a67b2dae6b0acafe58e5
ioana.frona <ioana.frona@b69a6be6-cbc9-11de-9d4f-f947ee5921c8>
2009-11-27 08:14:07 +0000

    "Message Logging Service" task completed

--- trunk/RAWI-Common/src/rawi/common/ValidateXMLInfo.java
+++ trunk/RAWI-Common/src/rawi/common/ValidateXMLInfo.java
@@ -1,12 +1,17 @@
 
 package rawi.common;
 
-public class ValidateXMLInfo {
+
+import java.io.Serializable;
+import java.rmi.RemoteException;
+
+public class ValidateXMLInfo implements Serializable {
     public boolean success;
     public String message;
     public int nodeID;
 
-    public ValidateXMLInfo(boolean success, String message, int nodeID) {
+    public ValidateXMLInfo(boolean success, String message, int nodeID)
+            throws RemoteException {
         this.success = success;
         this.message = message;
         this.nodeID = nodeID;
