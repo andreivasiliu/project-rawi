ca01d16c216051f796c49fd29504ed90e0cba462
ioana.frona <ioana.frona@b69a6be6-cbc9-11de-9d4f-f947ee5921c8>
2009-12-10 17:08:59 +0000

    Added StartSession RMI method to Main Server Interface

--- RAWI-Common/src/rawi/common/MainServerInterface.java
+++ RAWI-Common/src/rawi/common/MainServerInterface.java
@@ -4,8 +4,10 @@ import java.rmi.*;
 
 public interface MainServerInterface extends Remote
 {
-   ValidateXMLInfo validateXML(String xml)
-           throws RemoteException;
+    ValidateXMLInfo validateXML(String xml)
+            throws RemoteException;
+    void startSession(String download, String upload, String msgLog,
+            long sessionId) throws RemoteException;
 
     void taskCompleted(int id) throws RemoteException;
 }
