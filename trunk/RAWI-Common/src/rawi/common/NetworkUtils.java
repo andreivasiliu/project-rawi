057d479b4d394b3336e186c5947e4ba53ac50430
whyte.vuhuni <whyte.vuhuni@b69a6be6-cbc9-11de-9d4f-f947ee5921c8>
2009-12-16 18:43:46 +0000

    Moved some code from I-Prototype into NetworkUtils.

--- trunk/RAWI-Common/src/rawi/common/NetworkUtils.java
+++ trunk/RAWI-Common/src/rawi/common/NetworkUtils.java
@@ -4,13 +4,20 @@
  */
 package rawi.common;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.net.URL;
+import java.util.Collection;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 /**
  *
@@ -37,9 +44,25 @@ public class NetworkUtils {
         return list;
     }
 
-    //TODO  Get list of IP's of MainServer's
-    public static List<String> getIPsFromTracker(String type)
+    public static Collection<String> getIPsFromTracker(String type)
     {
-        return null;
+        Set<String> IPs = new HashSet<String>();
+
+        try {
+            URL servletURL = new URL
+                    ("http://testbot73.appspot.com/GetIPServlet?type=" + type);
+
+            BufferedReader in = new BufferedReader
+                    (new InputStreamReader(servletURL.openStream()));
+            String ip;
+
+            while ((ip = in.readLine()) != null) {
+                IPs.add(ip);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("IO Exception on getIPsFromTracker", e);
+        }
+
+        return IPs;
     }
 }
