6379512aa5953ceeb74d5b075766ad3bd2a2813f
andrei.arusoaie <andrei.arusoaie@b69a6be6-cbc9-11de-9d4f-f947ee5921c8>
2010-04-26 14:12:40 +0000

    WI-Prototype-GUI sources added.

---
+++ trunk/WI-Prototype-GUI/src/rawi/RAWIApp.java
@@ -0,0 +1,44 @@
+/*
+ * RAWIApp.java
+ */
+
+package rawi;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class RAWIApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new RAWIView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of RAWIApp
+     */
+    public static RAWIApp getApplication() {
+        return Application.getInstance(RAWIApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(RAWIApp.class, args);
+    }
+}
